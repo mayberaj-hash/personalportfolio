575f68876fea99d1860974ffd39b68f991e025da
Maybe Raj <98401591+mayberaj-hash@users.noreply.github.com>
2023-06-17 18:32:42 +0530

    Add files via upload

--- frontpage.js
+++
@@ -1,8 +0,0 @@
-const blob = document.getElementById("blob");
-
-document.body.onpointermove = event => {
-    const { clientx, clienty } = event;
-
-    blob.style.left = '${clientx}px';
-    blob.style.top = '${clienty}px';
-}
